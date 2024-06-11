0a4b57199f925c7a2a4997f11388fc44bc61c54e
Nikoleta <149585913+nikgisST@users.noreply.github.com>
2024-06-11 09:37:58 +0300

    Update Use the Python window for raster analysis.py
    Python window to develop a script to model a peregrine falcon habitat. Identify areas to set aside within the park for the peregrine falcon. These birds are considered a threatened species so consider potential habitat sites. The script is used to model the areas within the park that meet the habitat criteria.

--- Use the Python window for raster analysis.py
+++ Use the Python window for raster analysis.py
@@ -1,35 +1,38 @@
 import arcpy 
-
 from arcpy.sa import *     #importing Spatial Analyst tools
 
+# Load the raster data
 baseRaster = Raster('DEM_elkhorn.tif')     #creating the raster object
 
+# Get the raster properties
 baseRaster.maximum              #2856.9016113281 - raster object property
-
-baseRaster.minimum              #2174.0673828125 - raster object property
-
-baseRaster.noDataValue          #-3.4028230607370965e+38 - raster object property
-
+baseRaster.minimum              #2174.0673828125
+baseRaster.noDataValue          #-3.4028230607370965e+38
 baseRaster.uncompressedSize     #62886120 - For 8-bit per pixel data, uncompressed size can be found with this formula:
                                 #pixels X * pixels Y * number of bands = uncompressed bytes
 
-
+# Create the slope raster
 slopeRaster = Slope('DEM_elkhorn.tif', 'DEGREE')    #creating a slope raster and evaluate the values calculated
 
-gThan40 = slopeRaster > 40       #evaluating slope to determine the areas greater than 40 degrees.
+# Identify areas with slope greater than 40 degrees
+gThan40 = slopeRaster > 40                           #evaluating slope to determine the areas greater than 40 degrees.
  
-cliff = baseRaster - baseRaster.mean       #using the mean value from the baseRaster for the value to be subtracted from each raster cell value
+# Subtract the mean value from each cell value
+cliff = baseRaster - baseRaster.mean             #using the mean value from the baseRaster for the value to be subtracted from each raster cell value
 
-cliffPresent = GreaterThan(cliff, 160)     #identifying the potential cliffs by selecting all the areas above 160
+# Identify areas where the value is greater than 160
+cliffPresent = GreaterThan(cliff, 160)                 #identifying the potential cliffs by selecting all the areas above 160
 
-falconHabitat = gThan40 & cliffPresent     #after analyzed the digital elevation for all requirements, next is comparing both outputs to determine the potential habitat locations
+# Combine both criteria to identify potential falcon habitats
+falconHabitat = gThan40 & cliffPresent                             #after analyzed the digital elevation for all requirements, next is comparing both outputs to determine the potential habitat locations
 
+# Save the resulting raster to a file
 falconHabitat.save('C:\\EsriTraining\\PythonScriptsRaster\\PythRasterAnalysis\\Data\\HabitatOutput.tif')         #making the results permanent by saving the raster
 
 
 
 
+#You have now created all the portions of the script that you will need for the analysis. Can you combine them into one Python script command?
+#--> You can keep the raster object that you created in place for the solution
 
-# All the portions of the script are combined into one Python script command
-
-#falconHabitat2 = (Slope(baseRaster, 'DEGREE') > 40) & ((baseRaster-baseRaster.mean) > 160)
+falconHabitat2 = (Slope(baseRaster, 'DEGREE') > 40) & ((baseRaster-baseRaster.mean) > 160)
